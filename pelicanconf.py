4a11eb623e718e1dd65819242b6f5d43cd9de3ea
Brecht Machiels <brecht@mos6581.org>
2013-06-20 20:31:13 +0200

    page about Ph.D. thesis

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,4 +30,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+STATIC_PATHS = ['images', 'files']
+
 FILES_TO_COPY = (('extra/CNAME', 'CNAME'),)
+
