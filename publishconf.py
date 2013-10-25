2593d17db745c4d4b899fc09b22e90dd9bb35ecb
Brecht Machiels <brecht@mos6581.org>
2013-10-25 15:53:58 +0200

    Don't allow .git to be deleted

--- publishconf.py
+++ publishconf.py
@@ -17,6 +17,7 @@ FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
+OUTPUT_RETENTION = ['.git', '.gitignore']
 
 # Following items are often useful when publishing
 
