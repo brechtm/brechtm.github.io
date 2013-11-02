dc9ba15fe7ac9cfceb0f254587cd385ff2841648
Brecht Machiels <brecht@mos6581.org>
2013-11-02 15:24:31 +0100

    Upgrade to the new Elegant theme.

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,7 +20,7 @@ DIRECT_TEMPLATES = (('index', 'tags', 'categories','archives', 'search', '404'))
 
 TYPOGRIFY = True
 THEME = 'pelican-elegant'
-CUSTOM_CSS = 'customization'
+CUSTOM_CSS = True
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -38,7 +38,7 @@ AUTHOR_SAVE_AS = ''
 #)
 
 LANDING_PAGE_ABOUT = {
-    'title': 'Brecht Machiels, tech addict',
+    'title': 'Brecht Machiels <small>Tech Addict</small>',
     'details': """<p>I've always been intrigued by technology. My dad's <a href="http://en.wikipedia.org/wiki/Commodore_64" title="Wikipedia: Commodore 64">Commodore 64</a> grabbed my attention at an early age and computers and technology have never stopped fascinating me. I even went to the trouble of completing a <a href="/distributed-amplification-in-cmos" title="Distributed Amplification in CMOS">Ph.D. degree in micro-electronics</a>. Additionally, I am self-taught in computer programming, with over 10 years of hands-on experience. Over the years, I have developed a number of open-source applications. My most ambitious project so far, <a href="http://github.com/brechtm/rinohtype" title="RinohType">RinohType</a>, is a modern alternative to <a href="http://en.wikipedia.org/wiki/LaTeX" title="Wikipedia: LaTeX">LaTeX</a> written in <a href="http://www.python.org/" title="Python Programming Language - Official Website">Python</a>, my favorite programming language. Four years into its development, it is now quickly approaching a state where it offers most of the features you would expect from such an application.</p>
 
 <p>I'm very inquisitive; I want to know exactly how things work. I'm also perfectionist to the point where it aggravates even myself at times. I believe these two qualities help me be a good engineer; I'm only satisfied when I fully understand a problem and have devised an elegant solution. Otherwise, I can be summarized as an <a href="http://www.41q.com/type.41q?p=23582594&n=Brecht+Machiels" title="Brecht's personality type">individualistic doer</a> a.k.a. <a href="http://www.123test.com/ISTP-personality-type/" title="ISTP Personality Type">ISTP</a> (<a href="http://www.ted.com/talks/susan_cain_the_power_of_introverts.html" title="Susan Cain, The Power of Introverts">Introversion</a>, Sensing, Thinking, Perception), which has its advantages and, of course, disadvantages.</p>
@@ -81,6 +81,7 @@ LINKS = None
 GITHUB_URL = 'http://github.com/brechtm/'
 SOCIAL = (('twitter', 'http://twitter.com/brechtmachiels'),
           ('github', 'http://github.com/brechtm'),
+          ('email', 'mailto:brecht@mos6581.org'),
 )
 
 EMAIL = 'brecht@mos6581.org'
@@ -96,13 +97,13 @@ STATIC_PATHS = [
     'theme/images',
     'files',
     'extra/CNAME',
-    'extra/customization.css',
+    'extra/custom.css',
     'extra/google9ddc49a50caff71e.html'
 ]
 
 EXTRA_PATH_METADATA = {
     os.path.join('extra', 'CNAME'): {'path': 'CNAME'},
-    os.path.join('extra', 'customization.css'): {'path': 'theme/css/customization.css'},
+    os.path.join('extra', 'custom.css'): {'path': 'theme/css/custom.css'},
     os.path.join('extra', 'google9ddc49a50caff71e.html'): {'path': 'google9ddc49a50caff71e.html'},
 }
 
